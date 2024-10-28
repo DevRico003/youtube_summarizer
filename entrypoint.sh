b41140031bff34132db3d57fabefe2ea65a987d9
Enrico Carteciano <carteciano.enrico@gmail.com>
2024-10-28 21:53:42 +0100

    Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,5 @@
 # Start cron service
 service cron start
 
-# Run initial cookie setup
-/app/init_cookies.sh
-
-# Check if init_cookies.sh was successful
-if [ $? -eq 0 ]; then
     echo "Cookie initialization successful, starting Streamlit..."
-    streamlit run app.py --server.address 0.0.0.0
-else
-    echo "Cookie initialization failed. Check logs at /app/logs/initial_cookie_update.log"
-    exit 1
-fi
+    streamlit run app.py
